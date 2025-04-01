642cbbce3c40f5ebd1d050957d4e341768d87921
Mikku <51969732+Mikkukun@users.noreply.github.com>
2025-04-01 03:06:14 -0400

    Split chatbox and parameter port into distinct files

--- chatbox_manager.py
+++ chatbox_manager.py
@@ -2,9 +2,8 @@ from pythonosc.udp_client import SimpleUDPClient
 import time
 
 
-class OSCClient:
+class ChatboxManager:
     OSC_CHATBOX_PATH = "/chatbox/input"
-    OSC_LYRICS_PATH = "/Atomikku/VRCSpotifyOSC/Lyrics"
 
     PLAY_EMOJI = "\U000025B6"
     PAUSE_EMOJI = "\U000023F8"
@@ -17,19 +16,29 @@ class OSCClient:
         self.gui = gui
         self.track = None
         self.last_sent = ""
+        self.last_lyric = None
         self.last_update_time = time.time()
         self.refresh_timeout = 5
 
+    def send_osc_message(self, message):
+        self.client.send_message(self.OSC_CHATBOX_PATH, [message, True, False])
+        self.last_sent = message
+
     def handle_song_update(self):
-        self.gui.update_labels(self.track['name'], self.track['artists'][0]['name'], "Searching for Lyrics")
-        self.client.send_message(self.OSC_LYRICS_PATH, "")
+        self.gui.update_labels(self.track['name'], self.track['artists'][0]['name'], "")
 
     def handle_play_pause(self, is_playing):
         emoji = self.PLAY_EMOJI if is_playing else self.PAUSE_EMOJI
         song_display = f"{emoji} {self.track['name']} - {self.track['artists'][0]['name']}"
-        self.client.send_message(self.OSC_CHATBOX_PATH, [song_display, True, False])
-        self.gui.update_lyric("" if is_playing else "Paused")
-        self.last_sent = song_display
+
+        if self.last_lyric and is_playing:
+            song_display += f" \n {self.MIC_EMOJI} {self.last_lyric}"
+            self.gui.update_lyric(self.last_lyric)
+
+        if not is_playing:
+            self.gui.update_lyric("Paused")
+
+        self.send_osc_message(song_display)
 
     def handle_lyric_update(self, lyric):
         song_display = f"{self.PLAY_EMOJI} {self.track['name']} - {self.track['artists'][0]['name']}"
@@ -38,37 +47,36 @@ class OSCClient:
             song_display += f" \n {self.MIC_EMOJI} {lyric}"
 
         self.gui.update_lyric(lyric)
-        self.client.send_message(self.OSC_LYRICS_PATH, lyric)
-        self.client.send_message(self.OSC_CHATBOX_PATH, [song_display, True, False])
-        self.last_sent = song_display
+        self.send_osc_message(song_display)
+        self.last_lyric = lyric
 
     def process_queue_messages(self):
         while not self.song_data_queue.empty():
             message = self.song_data_queue.get_nowait()
             message_type = message['type']
-
             print(f"Got message: {message}")
 
             if message_type == 'song_update':
-                self.track = message['playback']
+                self.track = message['playback']['item']
                 self.handle_song_update()
+                self.handle_play_pause(message['playback']['is_playing'])
 
             elif message_type == 'is_playing':
                 self.handle_play_pause(message['is_playing'])
 
             elif message['type'] == 'lyric_update':
                 if message['lyric'] is None:
-                    self.gui.handle_lyric_update("Lyrics for this track are not available")
+                    self.gui.update_lyric("Lyrics for this track are not available")
                 self.handle_lyric_update(message['lyric'])
 
             self.last_update_time = time.time()
 
     def update(self):
-        if not self.track or self.track.get('message') == 'No track currently playing':
+        if not self.track:
             return
 
         if time.time() - self.last_update_time >= self.refresh_timeout:
-            self.client.send_message(self.OSC_CHATBOX_PATH, [self.last_sent, True, False])
+            self.send_osc_message(self.last_sent)
             self.last_update_time = time.time()
 
     def run(self):
@@ -77,6 +85,4 @@ class OSCClient:
             self.update()
             time.sleep(0.1)
 
-        # Handle shutdown
-        self.client.send_message(self.OSC_CHATBOX_PATH, ["Shutting Down", True, False])
-        self.client.send_message(self.OSC_LYRICS_PATH, "")
+        self.send_osc_message("Shutting Down...")
